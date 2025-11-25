8b991c1ddfdf35bab360d843b6c72b9fc03a39a8
Osman Muhina <Odyssey1universe@gmail.com>
2025-11-25 07:01:31 -0800

    Update App.js

--- stem-lessons/src/App.js
+++ stem-lessons/src/App.js
@@ -99,7 +99,7 @@ export default function App() {
   const runCode = async () => {
     setOutput("Running...");
     try {
-      const res = await fetch("http://127.0.0.1:8000/validate", {
+      const res = await fetch("http://https://codesim-gcpb.onrender.com/validate", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
